1521932873689be07bddf17f16d6bd51bd22c58f
edith.githinji <edithgithinji2020@gmail.com>
2024-11-21 19:37:02 +0300

    learning functions introduction

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Follow along with the examples here
+function sayHello (){
+    console.log("hello");
+}
+
+sayHello();
+
+function sayHelloTo (firstname){
+    console.log(`Hello ${firstname}`)
+}
+sayHelloTo("Edith")
+
+function say (greeting, firstname) {
+    console.log(`${greeting}, ${firstname}} !`)
+}
+
+say("Habibi, Edith")
+
+function sayMe(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+
+  sayMe()
